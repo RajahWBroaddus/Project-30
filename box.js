7b41aa372ede6fa09e3f40bd627ddaa38674b060
RajahWBroaddus <53542620+RajahWBroaddus@users.noreply.github.com>
2020-10-31 15:13:40 -0400

    Add files via upload

---
+++ box.js
@@ -0,0 +1,31 @@
+class Box {
+    constructor(x, y, width, height) {
+        var options = {
+            'restitiution': 0.0,
+            'friction': 1.0,
+            'density': 1.0,
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+
+        World.add(world, this.body);
+    }
+    display() {
+        if (this.body.speed < 5) {
+            var angle = this.body.angle;
+            var pos = this.body.position;
+            push();
+            translate(pos.x, pos.y);
+            rotate(angle);
+            rectMode(CENTER);
+            rect(0, 0, this.width, this.height);
+            pop();
+        } else {
+            console.log(this.body.speed);
+            World.remove(world, this.body);
+
+        }
+    }
+
+}
